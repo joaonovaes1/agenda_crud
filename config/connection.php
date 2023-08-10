dfbe93ae53f063b5254be13ccc8c309960f1cb35
João Marcelo Novaes <joaomnovaes1998@gmail.com>
2023-08-10 17:27:32 -0300

    first commit

---
+++ config/connection.php
@@ -0,0 +1,18 @@
+<?php 
+
+$host = "localhost";
+$db = "agenda";
+$user = "root";
+$pass = "";
+
+
+try{
+    $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch(PDOException $e){
+    // Erro na conexão
+    $error = $e->getMessage();
+    echo "Erro: " . $error;
+}
